78269e66f227677ddc5a61a377b48eaabc8fb3d2
ianzhang <ian.zhang@sv.cmu.edu>
2011-02-13 19:51:28 -0800

    update mapview

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -256,7 +256,6 @@ class ApiController < ApplicationController
       <![CDATA[
           <div style="width: 264px;max-height:250px;overflow:auto;">
             <div style="font-size: small; margin-top: 4px; ">
-            <p>An earthquake is caused by a sudden slip on a fault. Stresses in the earth’s outer layer push the sides of the fault together. Stress builds up and the rocks slips suddenly, releasing energy in waves that travel through the rock to cause the shaking that we feel during an earthquake.</p>
             <p>}+entity.description+%{</p>
             </div>
           </div>
