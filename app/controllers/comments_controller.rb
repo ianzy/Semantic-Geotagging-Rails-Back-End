b25fc51ad59f502d24bc1a8c39e5b184339f9efa
ianzhang <ian.zhang@sv.cmu.edu>
2011-06-09 18:23:41 -0700

    test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -41,7 +41,7 @@ class CommentsController < ApplicationController
   def show
     @comment = Comment.find(params[:id])
     @categories = CommentCategoryCounter.find_all_by_comment_id @comment,
-      :order=>"response_category_name DESC"
+      :order=>"response_category_name ASC"
     
     if @categories.empty?
         categories = ResponseCategory.find :all, :order=> 'created_at'
