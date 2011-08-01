35780fc9972aee8bee8ad504fad34edfdfe3d811
ianzhang <ian.zhang@sv.cmu.edu>
2011-08-01 13:50:18 -0700

    hmm

--- app/controllers/data_management_controller.rb
+++ app/controllers/data_management_controller.rb
@@ -1,9 +1,9 @@
 class DataManagementController < ApplicationController
   before_filter :require_admin
   def restore
-    dir = RAILS_ROOT + '/db/backup'
-    FileUtils.mkdir_p(dir)
-    FileUtils.chdir(dir)
+    # dir = RAILS_ROOT + '/db/backup'
+    # FileUtils.mkdir_p(dir)
+    # FileUtils.chdir(dir)
   
     interesting_tables.each do |tbl|
       tbl = "EntityCategoryCounter" if tbl == "comment_categories_entities"
@@ -16,7 +16,7 @@ class DataManagementController < ApplicationController
       ActiveRecord::Base.transaction do 
       
         puts "Loading #{tbl}..."
-        YAML.load_file("#{tbl}.yml").each do |fixture|
+        YAML.load_file("#{RAILS_ROOT}/db/backup/#{tbl}.yml").each do |fixture|
           ActiveRecord::Base.connection.execute "INSERT INTO #{tbl} (#{fixture.keys.join(",")}) VALUES (#{fixture.values.collect { |value| ActiveRecord::Base.connection.quote(value) }.join(",")})", 'Fixture Insert'
         end        
       end
